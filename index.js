6c9815ae91c0241eb900486826116b7a024c7832
Abhishek pandey <abhie9336@gmail.com>
2022-09-15 12:47:34 +0530

    completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const openBtn = document.querySelector('button');
 const closeBTn = document.querySelector('#close');
 const modal = document.querySelector('.modal');
+const form = document.querySelector('form');
 
 openBtn.addEventListener('click', opneModal);
 
@@ -10,7 +11,11 @@ function opneModal(){
 
 closeBTn.addEventListener('click', closeModal);
 function closeModal(){
-    modal.style.display = 'none';
+    modal.classList.add('close-modal-animation');
+    setTimeout(() => {
+        modal.style.display = 'none';
+        window.location.reload();
+    },500);
 }
 
 
@@ -19,3 +24,14 @@ modal.addEventListener('click', function(e){
         closeModal();
     }
 })
+
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(){
+    const card = document.querySelector('.card');
+    card.innerHTML = "<h1>Thanks for your Subscription!</h1>";
+    setTimeout(() => {
+        closeModal();
+    }, 3000);
+}
